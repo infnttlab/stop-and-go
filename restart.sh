f8dae777dff42d66b56d2128811d1dc1b21da26e
Elena <elena.corni@cnaf.infn.it>
2017-05-29 11:36:21 +0200

    fix

--- restart.sh
+++ restart.sh
@@ -2,5 +2,5 @@ echo $0 $1
 if [ $1 = "all-nodes" ]; then
  for A in `$PWD/show_running_hosts.sh|grep -v vm|grep -v avoton1|grep -v lab`; do echo ssh $A init 0; ssh $A init 0; done
 else
- for A in `cat $1|grep -v ipmi`; do echo ssh $A init 0; ssh $A init 0; done
+ for A in `cat $1|grep -v ipmi`; do echo ssh $A init 6; ssh $A init 6; done
 fi
--- show_running_hosts.sh
+++ show_running_hosts.sh
@@ -1,1 +1,5 @@
 nmap -sn 10.0.0.0/24|grep -v down|grep report|sort -n|awk '{print $5}'ls
+#for A in `nmap -sn 10.0.0.0/24|grep -v down|grep report|sort -n|awk '{print $5}'ls`
+#do
+#echo $A; ssh $A cat /etc/issue
+#done
